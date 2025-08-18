3da96a473b8c0aba69cd24d24ac7544c06f347af
dev-ramadan <ganna123adam@gmail.com>
2025-08-18 22:08:00 +0300

    test dashboard

--- src/supabasae/createclient.js
+++ src/supabasae/createclient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = import.meta.env.VITE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_ANON_KEY;
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsZnh5Ymt0cnR2dGRqcHVicXZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxNjQxODksImV4cCI6MjA3MDc0MDE4OX0.vSsgxe1HBLSKG48er3n9Ru0F6tBFnJBNjGSh0DdRknU';
+const SUPABASE_URL = 'https://ylfxybktrtvtdjpubqvn.supabase.co'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
